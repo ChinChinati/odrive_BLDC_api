afc587588a080d00fd039589326cd68c4bb996cc
Shailesh <106835636+ChinChinati@users.noreply.github.com>
2023-10-19 17:07:30 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -5,11 +5,8 @@ import time,sys
 
 
 odrv = odrive.find_any()
-print(str(odrv.vbus_voltage))
-
-
-odrv.config.dc_bus_overvoltage_trip_level = 50
-odrv.config.dc_bus_undervoltage_trip_level = 45
+odrv.config.dc_bus_overvoltage_trip_level = 15
+odrv.config.dc_bus_undervoltage_trip_level = 10.5
 odrv.config.dc_max_positive_current = 10
 odrv.config.dc_max_negative_current = -5
 odrv.axis0.config.motor.motor_type = MotorType.HIGH_CURRENT
@@ -20,8 +17,9 @@ odrv.axis0.config.motor.current_hard_max = 48
 odrv.axis0.config.motor.calibration_current = 10
 odrv.axis0.config.motor.resistance_calib_max_voltage = 2
 odrv.axis0.config.calibration_lockin.current = 10
-odrv.axis0.controller.config.input_mode = InputMode.PASSTHROUGH
 odrv.axis0.controller.config.control_mode = ControlMode.VELOCITY_CONTROL
+odrv.axis0.controller.config.input_mode = InputMode.VEL_RAMP
+odrv.axis0.controller.config.vel_ramp_rate = 1
 odrv.axis0.config.torque_soft_min = -0.40271304347826087
 odrv.axis0.config.torque_soft_max = 0.40271304347826087
 odrv.axis0.config.encoder_bandwidth = 100
@@ -33,19 +31,40 @@ odrv.axis0.config.commutation_encoder = EncoderId.HALL_ENCODER0
 def connect():
     odrv.clear_errors
     odrv.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
-    return 1
+    if odrv.axis0.current_state == AxisState.CLOSED_LOOP_CONTROL:
+        print("Connected")
+    else:
+        print("Error")
+        exit()
 
 def disconnect():
     odrv.axis0.requested_state = AxisState.IDLE
-    return 1
+    if odrv.axis0.current_state == AxisState.IDLE:
+        print("Disconnected")
+    else:
+        print("Error")
+        exit()
 
-def run(velocity = 0.5):
+def run(velocity = 0.7):
     odrv.axis0.controller.input_vel = velocity
+    if odrv.axis0.current_state == AxisState.CLOSED_LOOP_CONTROL:
+        print("Running")
+    elif odrv.axis0.current_state == AxisState.IDLE:
+        print("Error")
+        exit()
 
 def stop():
     odrv.axis0.controller.input_vel = 0
+    if odrv.axis0.current_state == AxisState.CLOSED_LOOP_CONTROL:
+        print("Stopped")
+    else:
+        print("Error")
+        exit()
 
-
+connect()
+run()
+time.sleep(10)
+stop()
 
 
 
